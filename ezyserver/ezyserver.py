de88241c22ba6cd735abb7ffd04d3a4c43d5525b
James Thomas <codeactor@protonmail.com>
2018-09-13 10:57:23 +0100

    add ezyserver

---
+++ ezyserver/ezyserver.py
@@ -0,0 +1,25 @@
+
+import http.server
+import socketserver
+
+PORT = 8080
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+Handler.extensions_map={
+        '.manifest': 'text/cache-manifest',
+	'.html': 'text/html',
+        '.png': 'image/png',
+	'.jpg': 'image/jpg',
+	'.svg':	'image/svg+xml',
+	'.css':	'text/css',
+	'.js':	'application/x-javascript',
+	'.wasm':'application/wasm',
+	'.json':'application/json',
+	'.xml':'application/xml',
+	'': 'application/octet-stream',
+    }
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
